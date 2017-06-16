768195a90da7e69a313233b2cb3db966930abe03
Justin Firth <jmfirth@gmail.com>
2017-06-16 15:15:05 -0400

    Updated scripts

--- package-postinstall.js
+++ package-postinstall.js
@@ -15,20 +15,7 @@ pkg.scripts['test'] = 'jest';
 pkg.scripts['validate'] = 'npm run lint && npm run check && npm run jest';
 pkg.scripts['postmerge'] = 'npm install';
 pkg.scripts['precommit'] = 'npm run validate';
-
-pkg.jest = {
-  "preset": "react-native",
-  "transform": {
-    "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
-    ".(ts|tsx)": "<rootDir>/node_modules/ts-jest/preprocessor.js"
-  },
-  "testRegex": "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
-  "moduleFileExtensions": [
-    "ts",
-    "tsx",
-    "js"
-  ]
-};
+pkg.scripts['postinstall'] = 'node node_modules/r-n-t-typescript-postinstal/template-postinstall.js';
 
 fs.writeFileSync(fileName, JSON.stringify(pkg, undefined, 2));
 
